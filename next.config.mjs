f96df36ced21f9f4a1d282ebc96b7106c4e4be8c
Brad Akemi <mirado.akemi@gmail.com>
2024-11-24 22:47:43 +0300

    serve

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   reactStrictMode: true,
+  images: {
+    unoptimized: true, // Désactiver l'optimisation des images
+  },
+  output: 'export',
 };
